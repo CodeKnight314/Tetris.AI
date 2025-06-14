c568a1292d6fe5ec147d4f2cf1ee6da7723f4b7a
CodeKnight314 <tang.ri@northeastern.edu>
2025-06-13 18:44:14 -0700

    Add number-episodes option

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ def main(args):
         if args.mode == "train":
             env.train(args.o)
         else: 
-            env.test(args.o)
+            env.test(args.o, args.num_episodes)
     except KeyboardInterrupt as e: 
         env.save_weights(args.o)
         env.close()
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     parser.add_argument("--seed", type=int, default=1898, help="Seed for experiment reproduciability")
     parser.add_argument("--mode", choices=["train", "test"], default="train", help="Mode to test/train Tetris Agent")
     parser.add_argument("--num_envs", type=int, help="Number of parallel environments to run at the same time")
-    
+    parser.add_argument("--num_episodes", type=int, help="Number of episodes to run for testing")
     args = parser.parse_args()
     
     main(args)
