4344218050d25e43496b3e9f90349d440f28ce40
CodeKnight314 <tang.ri@northeastern.edu>
2025-06-19 18:19:51 -0700

    Add feature integration and calculation for model; Add yaml config flow from env

--- src/agent.py
+++ src/agent.py
@@ -5,17 +5,22 @@ from torch.optim import AdamW
 import torch.nn as nn
 import numpy as np
 from typing import List
+from torch.optim.lr_scheduler import CosineAnnealingLR
 
 class TetrisAgent: 
     def __init__(self, 
                  frame_stack: int, 
                  ac_dim: int, 
                  lr: float = 1e-4, 
+                 min_lr: float = 1e-5,
                  gamma: float = 0.99, 
                  max_memory: int = 1000, 
                  max_gradient: float = 0.5, 
                  action_mask: List[int] = [5, 6], 
-                 buffer_type: str = "prioritized"):
+                 buffer_type: str = "prioritized",
+                 scheduler_max: int = 1000000,
+                 beta_start: int = 0.5,
+                 beta_frames: int = 10000):
         
         self.action_mask = action_mask
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -30,14 +35,15 @@ class TetrisAgent:
         self.target = TetrisAI(frame_stack, ac_dim).to(self.device)
         
         self.opt = AdamW(params=self.model.parameters(), lr=lr)
+        self.scheduler = CosineAnnealingLR(self.opt, scheduler_max, min_lr)
         
         self.criterion = nn.MSELoss(reduction="none")
         self.gamma = gamma
         self.action_dim = ac_dim
         self.max_grad = max_gradient
-        self.beta_start = 0.5
+        self.beta_start = beta_start
         self.beta = self.beta_start
-        self.beta_frames = 10000
+        self.beta_frames = beta_frames
         self.training_step = 0
 
         self.update_target_network(True)
@@ -62,12 +68,17 @@ class TetrisAgent:
             return np.random.choice(valid_actions)
         
         with torch.no_grad(): 
-            if not isinstance(state, torch.Tensor):
-                state = torch.as_tensor(state, dtype=torch.float32).unsqueeze(0).to(self.device)
+            _, height, width = state.shape
+            aggregate_height, bumpiness, max_height = self.calculate_height(state[0])
+            holes = self.calculate_holes(state[0])
+            features = torch.as_tensor(
+                np.array([holes/(height * width), bumpiness / (height * width / 2), aggregate_height / (height * width), max_height / (height)]), 
+                dtype=torch.float32
+            ).unsqueeze(0).to(self.device)
+            state = torch.as_tensor(state, dtype=torch.float32).unsqueeze(0).to(self.device)
             state = state.to(self.device)
             
-            q_values = self.model(state, normalize=False)
-            q_values_original = q_values.detach().clone()
+            q_values = self.model(state, features, normalize=True)
             
             if q_values.dim() == 1:
                 for action in self.action_mask:
@@ -77,16 +88,16 @@ class TetrisAgent:
                     q_values[0, action] = -float("inf")
 
             action = torch.argmax(q_values, dim=1).item()
-            return (action, q_values_original.sum().item())
+            return action
             
     def update(self, batch_size: int):
         self.training_step += 1
         
         if isinstance(self.buffer, PrioritizedReplayBuffer):
             self.beta = min(1.0, self.beta_start + self.training_step * (1.0 - self.beta_start) / self.beta_frames)
-            states, actions, rewards, next_states, dones, weights, indices = self.buffer.sample(batch_size, beta=self.beta)
+            states, actions, rewards, next_states, dones, weights, indices, features = self.buffer.sample(batch_size, beta=self.beta)
         else:
-            states, actions, rewards, next_states, dones = self.buffer.sample(batch_size)
+            states, actions, rewards, next_states, dones, features = self.buffer.sample(batch_size)
             weights = torch.ones_like(rewards, device=self.device)
             indices = None
         
@@ -95,16 +106,17 @@ class TetrisAgent:
         rewards = rewards.float().to(self.device)
         next_states = next_states.to(self.device)
         dones = dones.float().to(self.device)
+        features = features.float().to(self.device)
         
         with torch.no_grad():
-            next_actions = self.model(next_states, normalize=False).argmax(1, keepdim=True)
-            max_next_q = self.target(next_states, normalize=False).gather(1, next_actions)           
+            next_actions = self.model(next_states, features, normalize=True).argmax(1, keepdim=True)
+            max_next_q = self.target(next_states, features, normalize=True).gather(1, next_actions)           
             
             dones = dones.unsqueeze(-1)
             targets = rewards.unsqueeze(-1) + (1 - dones) * self.gamma * max_next_q
             targets = targets.squeeze(1).to(self.device)
 
-        current_q_values = self.model(states, normalize=False).gather(1, actions.unsqueeze(1)).squeeze(1)
+        current_q_values = self.model(states, features, normalize=True).gather(1, actions.unsqueeze(1)).squeeze(1)
         raw_loss = self.criterion(current_q_values, targets)
         weighted = weights * raw_loss
         loss = weighted.mean() 
@@ -113,13 +125,75 @@ class TetrisAgent:
         loss.backward()
         torch.nn.utils.clip_grad_norm_(self.model.parameters(), self.max_grad)
         self.opt.step()
+        self.scheduler.step()
 
         if isinstance(self.buffer, PrioritizedReplayBuffer):
             td_errors = targets.detach() - current_q_values.detach()
             new_prios = td_errors.abs().detach().cpu().numpy() + 1e-6
             self.buffer.update_priorities(indices, new_prios)
         
-        return loss.item()
+        q_value_mean = current_q_values.detach().cpu().numpy().mean() 
+        q_value_std = current_q_values.detach().cpu().numpy().std()
+        
+        return loss.item(), q_value_mean, q_value_std
     
-    def push(self, state, action, reward, next_state, done): 
-        self.buffer.push(state, action, reward, next_state, done)
+    def push(self, state, action, reward, next_state, done, features): 
+        self.buffer.push(state, action, reward, next_state, done, features)
+        
+    def calculate_holes(self, board: np.array):
+        holes = 0
+        for col in range(board.shape[1]):
+            for row in range(board.shape[0]-1):
+                if board[row, col] == 255 and board[row+1, col] == 0 and self.has_path_to_bottom(board, row, col):
+                    holes += board.shape[0] - row
+        return holes
+    
+    def has_path_to_bottom(self, board: np.array, start_row: int, start_column: int):
+        height, width = board.shape
+        visited = set()
+        stack = [(start_row, start_column)]
+        directions = [(1, 0), (1, -1), (1, 1)]
+        
+        while stack: 
+            row, col = stack.pop()
+            
+            if row == height: 
+                return True
+            
+            if (row, col) in visited or row < 0 or row >= height or col < 0 or col >= width:
+                continue
+            
+            if board[row, col] != 255: 
+                continue 
+            
+            for dir in directions: 
+                delta_y, delta_x = dir 
+                new_row = delta_y + row
+                new_col = delta_x + col
+                if (new_row, new_col) not in visited: 
+                    stack.append((new_row, new_col))
+                    
+        return False
+    
+    def calculate_height(self, board: np.array):
+        heights = np.zeros(board.shape[1])
+        for col in range(board.shape[1]):
+            highest_row = None
+            for row in range(board.shape[0]):
+                if board[row, col] == 255:
+                    highest_row = row
+                    break
+            
+            if highest_row is None:
+                heights[col] = 0
+                continue
+            
+            if self.has_path_to_bottom(board, highest_row, col):
+                heights[col] = board.shape[0] - highest_row
+            else:
+                heights[col] = 0
+    
+        aggregate_height = sum(heights)
+        bumpiness = sum(np.array([abs(heights[i] - heights[i+1]) for i in range(len(heights)-1)]))
+        max_height = np.max(heights)
+        return aggregate_height, bumpiness, max_height
